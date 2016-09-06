7e5c133c8591586a324215887ae8ac8b2b3f848e
Tran Duc Nhuan <tranducnhuan1994@gmail.com>
2016-09-06 22:35:16 +0700

    XGBoost Random

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -1,12 +1,12 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[29]:
 
 import pandas as pd
 import numpy as np
 from sklearn.feature_selection import SelectKBest
-from sklearn.cross_validation import StratifiedShuffleSplit, train_test_split
+from sklearn.cross_validation import StratifiedShuffleSplit, train_test_split, StratifiedKFold, cross_val_score
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.metrics import auc, f1_score, log_loss
 from keras.models import Sequential
@@ -14,23 +14,25 @@ from keras.layers import Dense, Activation
 from keras.objectives import categorical_crossentropy
 from keras.optimizers import Adam, Adagrad
 from keras.callbacks import EarlyStopping
+from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils.np_utils import to_categorical
 
 
-# In[19]:
+# In[58]:
 
 train_dat = pd.read_csv('data/train.csv')
 labelEncoder = LabelEncoder()
 onehotEncoder = OneHotEncoder()
 train_dat.head()
 new_dat = np.log(train_dat.drop(['id','target'],axis=1)+1)
+kfold = StratifiedKFold(labelEncoder.fit_transform(train_dat.target[:X_train.shape[0]]), n_folds=10, shuffle=True)
 categories = labelEncoder.fit_transform(train_dat.target).reshape(-1,1)
 onehot_categorical = onehotEncoder.fit_transform(categories.reshape(-1,1)).toarray()
 dummy_y = to_categorical(categories)
 X_train,X_test,y_train, y_test = train_test_split(new_dat,onehot_categorical,test_size=0.3,stratify=categories)
 
 
-# In[20]:
+# In[71]:
 
 hidden_nodes = 10
 output_node = labelEncoder.classes_.shape[0]
@@ -42,14 +44,43 @@ model = Sequential([
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-# In[21]:
+# In[60]:
+
+def create_model():
+    hidden_nodes = 10
+    output_node = labelEncoder.classes_.shape[0]
+    model = Sequential([
+        Dense(hidden_nodes,input_dim=X_train.shape[1], activation='sigmoid',init='uniform'),
+        Dense(output_node, init='uniform'),
+        Activation('softmax')
+    ])
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    return model
+
+
+# In[61]:
 
 model.summary()
 
 
-# In[ ]:
+# In[65]:
 
-history = model.fit(X_train.values,y_train, nb_epoch=200, batch_size=20, verbose=2)
+sk_model = KerasClassifier(build_fn=create_model,nb_epoch=10, batch_size = 10)
+
+
+# In[66]:
+
+scores = cross_val_score(sk_model, X_train.values,y_train, cv=kfold, n_jobs=1, scoring='log_loss')
+
+
+# In[69]:
+
+scores.mean()
+
+
+# In[70]:
+
+# history = model.fit(X_train.values,y_train, nb_epoch=200, batch_size=20, verbose=2, validation_data=(X_test.values,y_test))
 
 
 # In[25]:
